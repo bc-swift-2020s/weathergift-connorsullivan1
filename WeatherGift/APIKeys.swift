6bcee997438c6fa622cf7cd5573a7da6c111219a
Connor Sullivan <sulliamr@bc.edu>
2020-03-21 13:36:12 -0400

    with .gitignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Connor on 3/21/20.
+//  Copyright © 2020 Connor Sullivan. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyC3qucvzhVFXDgzyCVxwkpj-g859NRRYdo"
+}
